6dacec3e636a672e23282cf6f8e228bf38d8df7a
Kushagra Sharma <kushagra8888@gmail.com>
2015-11-29 02:26:13 +0530

    add skeltal rewards page

--- cats/controllers/index.js
+++ cats/controllers/index.js
@@ -16,4 +16,12 @@ module.exports = function (router) {
 
     });
 
+    router.get('/rewards', passport.authenticate('basic'), function (req, res) {
+
+        res.render('rewards', model);
+
+    });
+
+
+
 };
