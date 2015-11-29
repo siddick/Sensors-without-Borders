63e5d023ae27e87f1e223116d0e879676ad8cc27
prakash14india <prakash14india@gmail.com>
2015-11-29 15:09:52 +0530

    Update

--- cats/public/js/app.js
+++ cats/public/js/app.js
@@ -23,6 +23,27 @@ require(['bootstrap', 'tableTools'], function () {
     var app = {
         initialize: function () {
 			$('table.table').DataTable();
+
+			//Loading Batches
+			var earnOverAll = parseInt($("#earnOverAll").html());
+			var yetToEarnOverAll = parseInt($("#yetToEarnOverAll").html());
+			var karmaInstallEarn = parseInt($("#karmaInstallEarn").html());
+			var karmaInstallationPending = parseInt($("#karmaInstallationPending").html());
+			var karmaMaintenanceEarn = parseInt($("#karmaMaintenanceEarn").html());
+			var karmaMaintenancePending = parseInt($("#karmaMaintenancePending").html());
+			if (earnOverAll >= 10000) {
+				$("#badge").attr("src", "../badges/ninja.jpg");
+			} else if (earnOverAll >= 5000) {
+				$("#badge").attr("src", "../badges/prodigy.jpg");
+			} else if (earnOverAll >= 2000) {
+				$("#badge").attr("src", "../badges/space_jammer.jpg");
+			} else if (earnOverAll >= 1000) {
+				$("#badge").attr("src", "../badges/book_worm.jpg");
+			} else {
+				$("#badge").attr("src", "../badges/couch_potato.jpg");
+			}
+
+			
         }
     };
 
--- cats/public/js/charts/chart.js
+++ cats/public/js/charts/chart.js
@@ -1,14 +1,9 @@
-google.load('visualization', '1', {packages: ['corechart', 'bar']});
-google.setOnLoadCallback(drawAnnotations);
-
-function drawAnnotations() {
 	var earnOverAll = parseInt($("#earnOverAll").html());
 	var yetToEarnOverAll = parseInt($("#yetToEarnOverAll").html());
 	var karmaInstallEarn = parseInt($("#karmaInstallEarn").html());
 	var karmaInstallationPending = parseInt($("#karmaInstallationPending").html());
 	var karmaMaintenanceEarn = parseInt($("#karmaMaintenanceEarn").html());
 	var karmaMaintenancePending = parseInt($("#karmaMaintenancePending").html());
-
 	if (earnOverAll >= 10000) {
 		$("#badge").attr("src", "../badges/ninja.jpg");
 	} else if (earnOverAll >= 5000) {
@@ -21,43 +16,3 @@ function drawAnnotations() {
 		$("#badge").attr("src", "../badges/couch_potato.jpg");
 	}
 
-
-	var data = google.visualization.arrayToDataTable([
-		['Device', 'Earned', 'Pending'],
-		['Installation', karmaInstallEarn, karmaInstallationPending],
-		['Maintenance', karmaMaintenanceEarn, karmaMaintenancePending],
-		['Overall', earnOverAll, yetToEarnOverAll]
-		]);
-
-	var options = {
-title: '',
-chartArea: {width: '50%'},
-annotations: {
-alwaysOutside: true,
-textStyle: {
-fontSize: 12,
-auraColor: 'none',
-color: '#555'
-	       },
-boxStyle: {
-stroke: '#ccc',
-strokeWidth: 1,
-gradient: {
-color1: '#f3e5f5',
-color2: '#f3e5f5',
-x1: '0%', y1: '0%',
-x2: '100%', y2: '100%'
-	}
-	       }
-       },
-hAxis: {
-title: 'Points',
-minValue: 0,
-       },
-vAxis: {
-title: 'Process'
-       }
-	};
-	var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-	chart.draw(data, options);
-}
